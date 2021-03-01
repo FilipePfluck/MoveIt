f24d65d31ff31020052fa336e6e0145931713eb4
Filipe Pfluck <filipepfluckdev@gmail.com>
2021-02-28 23:05:13 -0300

    integrating prisma

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,7 +1,13 @@
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
 
+import Adapters from 'next-auth/adapters'
+import { PrismaClient } from '@prisma/client'
+
+
 import { NextApiHandler } from 'next'
+
+const prisma = new PrismaClient()
 
 const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
 export default authHandler;
@@ -14,5 +20,6 @@ const options = {
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
-  ]
+  ],
+  adapter: Adapters.Prisma.Adapter({ prisma }),
 };
--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,14 +1,26 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { GetServerSideProps, NextPage } from 'next'
+import { getSession } from 'next-auth/client'
+
+import { PrismaClient } from '@prisma/client'
 
 import Sidebar from '../../components/SideBar'
 import Dashboard from '../../components/Dashboard'
 import Leaderboard from '../../components/LeaderBoard'
 
 import * as S from './styles'
+import { User } from 'next-auth'
 
+interface Props {
+    users: User[]
+}
 
-const Home = () => {
+const Home: NextPage<Props> = ({users}) => {
     const [selected, setSelected] = useState('dashboard')
+
+    useEffect(()=>{
+        console.log('a',users)
+    },[users])
 
     return(
         <S.Wrap>
@@ -19,6 +31,28 @@ const Home = () => {
 
         </S.Wrap>
     )
+}
+
+export const getServerSideProps: GetServerSideProps<Props> = async({req, res}) => {
+
+    const prisma = new PrismaClient()
+
+    const session = await getSession()
+
+    if (!session) {
+        res.statusCode = 403
+        return { props: { users: [] } }
+      }
+
+    const users = await prisma.user.findMany()
+
+    console.log(users)
+
+    return{
+        props: {
+            users
+        }
+    }
 }
 
 export default Home
